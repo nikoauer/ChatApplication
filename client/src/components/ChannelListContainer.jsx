e90c4d8772f964886d6dccd870ccdd519dd57311
Niko Auer <nikoauer26@gmail.com>
2024-06-22 18:33:54 +1000

    added company header with logo

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -4,12 +4,13 @@ import { ChannelList, useChatContext } from 'stream-chat-react'
 import Cookies from 'universal-cookie'
 import chatSymbol from '../assets/chatSymbol.png'
 import signOut from '../assets/signout.png'
+import chatAppLogo from '../assets/ChatAppLogo.png'
 
 const SideBar = () => (
-    <div className="w-18 shadow-md bg-purple-500">
+    <div className="w-18 shadow-md bg-purple-600">
         <div className="w-[44px] h-[44px] m-[14px] rounded-full shadow-md">
             <div className="font-sans h-full flex items-center justify-center">
-                <img src={chatSymbol} alt="chat Logo" width="50"/>
+                <img src={chatSymbol} alt="chat symbol" width="50"/>
             </div>
         </div>
         <div className="w-[44px] h-[44px] m-[14px] rounded-full shadow-md">
@@ -20,10 +21,19 @@ const SideBar = () => (
     </div>
 )
 
+const CompanyHeader = () => (
+    <div className='px-5 h-[62px]'>
+        <img src={chatAppLogo} alt="Logo" width="200"/>
+    </div>
+)
+
 const ChannelListContainer = () => {
   return (
     <div className="h-screen flex">
         <SideBar/>
+        <div className='flex justify-center bg-purple-500'>
+            <CompanyHeader />
+        </div>
     </div>
   )
 }
