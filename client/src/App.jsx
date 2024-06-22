f96b35739afad7a3ba0449d679a5b2d01794381b
Niko Auer <nikoauer26@gmail.com>
2024-06-22 12:47:05 +1000

    created channel list container and channel container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+import ChannelContainer from "./components/ChannelContainer";
+import ChannelListContainer from "./components/ChannelListContainer";
+
+const apiKey = process.env.REACT_APP_API_KEY
+
+const client = StreamChat.getInstance(apiKey)
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+return(
+    <div class="flex flex-1 h-full shadow-md">
+        <Chat client={client} theme="team light">
+            <ChannelListContainer 
+            
+            />
+            <ChannelContainer 
+            
+            />
+        </Chat>
+    </div>
+)
+};
 
-export default App
+export default App;
