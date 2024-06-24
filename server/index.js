150d1bd56e0f2586b0a0041d1c5925c3327d0431
Niko Auer <nikoauer26@gmail.com>
2024-06-24 21:12:50 +1000

    created basic index and auth routes js

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const authRoutes = require("./routes/auth.js")
+
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/", (req, res) => {
+  res.send('Hello there')
+});
+
+app.use('/auth', authRoutes)
+
+app.listen(PORT, () => console.log(`Server is live and running on ${PORT}`))
