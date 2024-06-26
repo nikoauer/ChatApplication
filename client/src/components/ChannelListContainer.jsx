433eb5ed762cf8a3fe9997e83f0f64c2c7194c23
Niko Auer <nikoauer26@gmail.com>
2024-06-26 20:55:29 +1000

    added logout functionality to sidebar

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -10,7 +10,9 @@ import chatSymbol from "../assets/chatSymbol.png";
 import signOut from "../assets/signout.png";
 import chatAppLogo from "../assets/ChatAppLogo.png";
 
-const SideBar = () => (
+const cookies = new Cookies();
+
+const SideBar = ({logout}) => (
   <div className="w-18 shadow-md bg-indigo-600">
     <div className="w-[44px] h-[44px] m-[14px] rounded-full shadow-md">
       <div className="font-sans h-full flex items-center justify-center">
@@ -18,7 +20,7 @@ const SideBar = () => (
       </div>
     </div>
     <div className="w-[44px] h-[44px] m-[14px] rounded-full shadow-md">
-      <div className="font-sans h-full flex items-center justify-center">
+      <div className="font-sans h-full flex items-center justify-center cursor-pointer" onClick={logout}>
         <img src={signOut} alt="signout" width="50" />
       </div>
     </div>
@@ -32,9 +34,22 @@ const CompanyHeader = () => (
 );
 
 const ChannelListContainer = () => {
+
+  const logout = () => {
+    cookies.remove("token");
+    cookies.remove("userId");
+    cookies.remove("username");
+    cookies.remove("fullName");
+    cookies.remove("avatarURL");
+    cookies.remove("hashedPassword");
+    cookies.remove("phoneNumber");
+
+    window.location.reload()
+  }
+
   return (
     <div className="h-screen flex">
-      <SideBar />
+      <SideBar logout={logout}/>
       <div className="flex flex-col justify-start bg-indigo-500 px-3 w-full">
         <CompanyHeader />
         <div className="flex-grow overflow-auto">
