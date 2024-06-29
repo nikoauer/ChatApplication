a33e9dc39d11c53f08efe5f5efcd53e7a649f142
Niko Auer <nikoauer26@gmail.com>
2024-06-29 14:33:22 +1000

    changed channel list container to render dynamic menus for smaller screens, requires tweaking

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,10 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { Dialog, DialogBackdrop, DialogPanel, TransitionChild } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { ChannelList, useChatContext } from "stream-chat-react";
-import {
-  ChannelSearch,
-  TeamChannelList,
-  TeamChannelPreview,
-} from "./indexComponents";
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./indexComponents";
 import Cookies from "universal-cookie";
 import chatSymbol from "../assets/chatSymbol.png";
 import signOut from "../assets/signout.png";
@@ -12,7 +10,7 @@ import chatAppLogo from "../assets/ChatAppLogo.png";
 
 const cookies = new Cookies();
 
-const SideBar = ({logout}) => (
+const SideBar = ({ logout }) => (
   <div className="w-18 shadow-md bg-indigo-600">
     <div className="w-[44px] h-[44px] m-[14px] rounded-full shadow-md">
       <div className="font-sans h-full flex items-center justify-center">
@@ -28,12 +26,13 @@ const SideBar = ({logout}) => (
 );
 
 const CompanyHeader = () => (
-  <div className="px-5 h-[62px]">
+  <div className="px-5 pt-3 h-[62px]">
     <img src={chatAppLogo} alt="Logo" width="200" />
   </div>
 );
 
 const ChannelListContainer = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   const logout = () => {
     cookies.remove("token");
@@ -44,17 +43,74 @@ const ChannelListContainer = () => {
     cookies.remove("hashedPassword");
     cookies.remove("phoneNumber");
 
-    window.location.reload()
-  }
+    window.location.reload();
+  };
 
   return (
-    <div className="h-screen flex">
-      <SideBar logout={logout}/>
-      <div className="flex flex-col justify-start bg-indigo-500 px-3 w-full">
-        <CompanyHeader />
-        <div className="flex-grow overflow-auto">
-          <ChannelSearch />
-          <div className="mt-3">
+    <div>
+      {/* Dynamic sidebar */}
+      <Dialog className="relative z-50 lg:hidden" open={sidebarOpen} onClose={setSidebarOpen}>
+        <DialogBackdrop
+          transition
+          className="fixed inset-0 bg-gray-900/80 transition-opacity duration-300 ease-linear data-[closed]:opacity-0"
+        />
+
+        <div className="fixed inset-0 flex">
+          <DialogPanel
+            transition
+            className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-[closed]:-translate-x-full"
+          >
+            <TransitionChild>
+              <div className="absolute left-full top-0 flex w-16 justify-center pt-5 duration-300 ease-in-out data-[closed]:opacity-0">
+                <button type="button" className="-m-2.5 p-2.5" onClick={() => setSidebarOpen(false)}>
+                  <span className="sr-only">Close sidebar</span>
+                  <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                </button>
+              </div>
+            </TransitionChild>
+            <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-2">
+              <CompanyHeader />
+              <div className="flex-1 flex flex-col gap-y-4">
+                <ChannelSearch />
+                <ChannelList
+                  filters={{}}
+                  channelRenderFilterFn={() => {}}
+                  List={(listProps) => (
+                    <TeamChannelList {...listProps} type="team" />
+                  )}
+                  Preview={(previewProps) => (
+                    <TeamChannelPreview {...previewProps} type="team" />
+                  )}
+                />
+                <ChannelList
+                  filters={{}}
+                  channelRenderFilterFn={() => {}}
+                  List={(listProps) => (
+                    <TeamChannelList {...listProps} type="messaging" />
+                  )}
+                  Preview={(previewProps) => (
+                    <TeamChannelPreview {...previewProps} type="messaging" />
+                  )}
+                />
+              </div>
+              <div className="mt-auto mb-4">
+                <div className="w-[44px] h-[44px] mx-auto rounded-full shadow-md">
+                  <div className="font-sans h-full flex items-center justify-center cursor-pointer" onClick={logout}>
+                    <img src={signOut} alt="signout" width="50" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </DialogPanel>
+        </div>
+      </Dialog>
+      
+      {/* Static Sidebar */}
+      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+        <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-2">
+          <CompanyHeader />
+          <div className="flex-1 flex flex-col gap-y-4">
+            <ChannelSearch />
             <ChannelList
               filters={{}}
               channelRenderFilterFn={() => {}}
@@ -62,12 +118,9 @@ const ChannelListContainer = () => {
                 <TeamChannelList {...listProps} type="team" />
               )}
               Preview={(previewProps) => (
-                <TeamChannelPreview {...previewProps} 
-                type="team"
-                />
+                <TeamChannelPreview {...previewProps} type="team" />
               )}
             />
-
             <ChannelList
               filters={{}}
               channelRenderFilterFn={() => {}}
@@ -75,16 +128,32 @@ const ChannelListContainer = () => {
                 <TeamChannelList {...listProps} type="messaging" />
               )}
               Preview={(previewProps) => (
-                <TeamChannelPreview {...previewProps} 
-                type="messaging"
-                />
+                <TeamChannelPreview {...previewProps} type="messaging" />
               )}
             />
           </div>
+          <div className="mt-auto mb-4">
+            <div className="w-[44px] h-[44px] mx-auto rounded-full shadow-md">
+              <div className="font-sans h-full flex items-center justify-center cursor-pointer" onClick={logout}>
+                <img src={signOut} alt="signout" width="50" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+
+      <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-indigo-600 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
+        <button type="button" className="-m-2.5 p-2.5 text-indigo-200 lg:hidden" onClick={() => setSidebarOpen(true)}>
+          <span className="sr-only">Open sidebar</span>
+          <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+        </button>
+      </div>
+
+      <main className="py-10 lg:pl-72">
+      </main>
     </div>
   );
 };
 
 export default ChannelListContainer;
+
