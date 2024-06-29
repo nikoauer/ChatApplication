ae4a912531077d02872dd6b5b4e12d6ba410ebd5
Niko Auer <nikoauer26@gmail.com>
2024-06-29 11:05:41 +1000

    added basic channel comntainer code, however need to sort out bugs

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -1,48 +1,43 @@
-import React from 'react'
-import {Channel, useChatContext} from 'stream-chat-react'
-import {ChannelInner, CreateChannel, EditChannel, TeamMessage} from './indexComponents'
+import React from 'react';
+import { MessageSimple, useChatContext, Channel } from 'stream-chat-react';
+import { ChannelInner, CreateChannel, EditChannel, TeamMessage } from './indexComponents';
 
-const ChannelContainer = (
-{  isCreating,
-  setIsCreating,
-  createType,
-  setIsEditing,
-  isEditing}
-) => {
-const {Channel} = useChatContext()
+const ChannelContainer = ({ isCreating, setIsCreating, createType, setIsEditing, isEditing }) => {
+  const { channel } = useChatContext(); // rename Channel to channel to avoid conflict
 
-if(isCreating) {
-  return(
-    <div className="h-full w-full">
-      <CreateChannel createType={createType} setIsCreating={setIsCreating}/>
+  if (isCreating) {
+    return (
+      <div className="h-full w-full">
+        <CreateChannel createType={createType} setIsCreating={setIsCreating} />
+      </div>
+    );
+  }
+  
+  if (isEditing) {
+    return (
+      <div className="h-full w-full">
+        <EditChannel setIsEditing={setIsEditing} />
+      </div>
+    );
+  }
+
+  const EmptyState = () => (
+    <div>
+      <p className="">This is the beginning of your chat history</p>
+      <p className="">Send Messages, emoji's, links and more! 😁</p>
     </div>
-  )
-}
-if(isEditing) {
-  return(
-    <div className="h-full w-full">
-    <EditChannel setIsEditing={setIsEditing}/>
-  </div>
-  )
-}
-
-const EmptyState = () => (
-  <div>
-    <p className=''>This is the beginning of your chat history</p>
-    <p className=''>Send Messages, emoji's, links and more! 😁</p>
-  </div>
-)
+  );
 
   return (
     <div className="h-full w-full">
       <Channel 
         EmptyStateIndicator={EmptyState}
-        Message={(messageProps, i) => <TeamMessage key={i} {...messageProps}/>}
+        Message={(messageProps, i) => <MessageSimple key={i} {...messageProps} />}
       >
-        <ChannelInner setIsEditing={setIsEditing}/>
+        <ChannelInner setIsEditing={setIsEditing} />
       </Channel>
     </div>
-  )
+  );
 }
 
-export default ChannelContainer
+export default ChannelContainer;
