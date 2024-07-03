25d82bcb480443aeca8b8376d102d0e19a085d60
Niko Auer <nikoauer26@gmail.com>
2024-07-03 21:23:54 +1000

    created createchannel component and userlist

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useChatContext } from 'stream-chat-react'
+
+// import { CloseCreateChannel } from '../assets'
+// import { UserList } from './indexComponents'
+
+const ChannelNameInput = ({ channelName = '', setChannelName }) => {
+  const handleChange = (event) => {
+    event.preventDefault()
+    setChannelName(event.target.value)
+  }
+
+  return (
+    <div>
+      <p>Name</p>
+      <input value={channelName} onChange={handleChange} placeholder="Channel-Name (With no spaces)" />
+      <p>Add Members</p>
+    </div>
+  )
+}
 
 const CreateChannel = () => {
   return (
-    <div>CreateChannel</div>
+    <div>
+      <ChannelNameInput />
+    </div>
   )
 }
 
