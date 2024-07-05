b6e63852a4f401c0456f73072c44808ddfac121d
Niko Auer <nikoauer26@gmail.com>
2024-07-05 10:42:51 +1000

    added some styling to createchannel

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -11,10 +11,10 @@ const ChannelNameInput = ({ channelName = '', setChannelName }) => {
   }
 
   return (
-    <div>
-      <p>Name</p>
-      <input value={channelName} onChange={handleChange} placeholder="Channel-Name (With no spaces)" />
-      <p>Add Members</p>
+    <div className='flex flex-col h-[169px] px-5 py-10 shadow-sm'>
+      <p className='font-medium text-lg pl-1'>Name</p>
+      <input value={channelName} onChange={handleChange} placeholder="Channel-Name" className='p-1' />
+      <p className='pl-1'>Add Members</p>
     </div>
   )
 }
@@ -22,7 +22,7 @@ const ChannelNameInput = ({ channelName = '', setChannelName }) => {
 const CreateChannel = () => {
   return (
     <div>
-      <ChannelNameInput />
+
     </div>
   )
 }
