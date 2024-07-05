381f1249df670ab130517717abbbd860965696e1
Niko Auer <nikoauer26@gmail.com>
2024-07-05 13:16:47 +1000

    added styling for users and added toggle for add user

--- client/src/assets/CloseCreateChannel.jsx
+++ client/src/assets/CloseCreateChannel.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const CloseCreateChannel = ({ setIsCreating, setIsEditing }) => (
-<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="size-11 cursor-pointer"
+<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="size-11 cursor-pointer"
     width='32'
     height='32'
     viewBox='0 0 32 32'
@@ -10,7 +10,7 @@ export const CloseCreateChannel = ({ setIsCreating, setIsEditing }) => (
       if (setIsEditing) setIsEditing(false);
     }}
   >
-  <path className='fill-indigo-700' fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z" clip-rule="evenodd" />
+  <path className='fill-indigo-700' fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z" clipRule="evenodd" />
   </svg>
 );
 
--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,74 +1,99 @@
-import React, {useEffect, useState} from 'react'
-import {Avatar, useChatContext} from 'stream-chat-react'
-import {InviteIcon} from '../assets/InviteIcon'
+import React, { useEffect, useState } from "react";
+import { Avatar, useChatContext } from "stream-chat-react";
+import { InviteIcon } from "../assets/InviteIcon";
+import { AiOutlineUserAdd } from "react-icons/ai";
 
-const ListContainer = ({children}) => {
+
+const ListContainer = ({ children }) => {
   return (
-    <div className='flex flex-col h-full'>
-      <div className='flex items-center justify-between mx-6 my-4'>
-        <div className='flex items-center flex-2'>
-          <p className='font-bold'>User</p>
-          <p className='ml-6 font-bold'>Name/ID</p>
+    <div className="flex flex-col h-full">
+      <div className="flex items-center justify-between mx-6 my-4">
+        <div className="flex items-center flex-2">
+          <p className="font-bold">User</p>
+          <p className="ml-6 font-bold">Name/ID</p>
         </div>
-        <p className='font-bold'>Invite</p>
+        <p className="font-bold">Invite</p>
       </div>
       {children}
     </div>
-  )
-}
+  );
+};
 
-const UserItem = ({user}) => {
+const UserItem = ({ user }) => {
+  const [selected, setSelected] = useState(false);
+
+  const handleSelect = () => {
+    setSelected((prevSelected) => !prevSelected);
+  };
+
+  const truncateId = (id, n) => {
+    return (id && id.length > n) ? id.substr(0, n) + '...' : id;
+  };
+
   return (
-    <div className='flex items-center mx-6 justif-between py-1'>
-      <div className='flex items-center flex-2 text-left'>
-        <Avatar image={user.image} name={user.fullName || user.id} size={40}/>
-        <p className='font-normal text-md pl-3'>{user.fullName || user.id}</p>
+    <div
+      className="flex items-center px-6 justify-between py-1 hover:bg-slate-100"
+      onClick={handleSelect}
+    >
+      <div className="flex items-center flex-2 text-left">
+        <Avatar image={user.image} name={user.fullName || user.id} size={40} />
+        <p className="font-normal text-md pl-3">{user.fullName || truncateId(user.id, 14)}</p>
       </div>
-      <InviteIcon />
+      <div className="pr-2">
+        {selected ? (
+          <div className="flex items-center justify-center h-10 w-10 bg-indigo-600 text-white rounded-full box-border">
+            <AiOutlineUserAdd className="h-6 w-6" />
+          </div>
+        ) : (
+          <div className="flex items-center justify-center h-10 w-10 bg-gray-200 border border-gray-200 rounded-full box-border ml-0.5">
+            <AiOutlineUserAdd className="h-6 w-6 text-gray-400" />
+          </div>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 const UserList = () => {
-  const {client} = useChatContext()
-  const [user, setUsers] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [listEmpty, setListEmpty] = useState(false)
+  const { client } = useChatContext();
+  const [user, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [listEmpty, setListEmpty] = useState(false);
 
   useEffect(() => {
     const getUsers = async () => {
-      if(loading) return
+      if (loading) return;
 
-      setLoading(true)
+      setLoading(true);
 
       try {
         const response = await client.queryUsers(
-          {id: {$ne: client.userID}},
-          {id: 1},
-          {limit: 8}
-        )
-        if(response.users.length) {
-          setUsers(response.users)
+          { id: { $ne: client.userID } },
+          { id: 1 },
+          { limit: 8 }
+        );
+        if (response.users.length) {
+          setUsers(response.users);
         } else {
-          setListEmpty(true)
+          setListEmpty(true);
         }
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-      setLoading(false)
-    }
-    if(client) getUsers()
-  }, [])
+      setLoading(false);
+    };
+    if (client) getUsers();
+  }, []);
 
   return (
     <ListContainer>
-      {loading ? <div className='font-sm m-20'>Loading Users...</div> : (
-        user?.map((user, i) => (
-          <UserItem index={i} key={user.id} user={user}/>
-        ))
+      {loading ? (
+        <div className="font-sm m-20">Loading Users...</div>
+      ) : (
+        user?.map((user, i) => <UserItem index={i} key={user.id} user={user} />)
       )}
     </ListContainer>
-  )
-}
+  );
+};
 
-export default UserList
+export default UserList;
