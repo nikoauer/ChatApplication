d33f8525a7030809bad92b5fadfc36f88fbd5ed1
Niko Auer <nikoauer26@gmail.com>
2024-07-05 11:55:58 +1000

    added code to userlist to display users to invite

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,11 +1,11 @@
 import React, {useEffect, useState} from 'react'
-import {Avatar, useChatChannel} from 'stream-chat-react'
+import {Avatar, useChatContext} from 'stream-chat-react'
 import {InviteIcon} from '../assets/InviteIcon'
 
 const ListContainer = ({children}) => {
   return (
     <div className='flex flex-col h-full'>
-      <div className='flex align-middle justify-between mx-6 my-4'>
+      <div className='flex items-center justify-between mx-6 my-4'>
         <p>User</p>
         <p>Invite</p>
       </div>
@@ -14,10 +14,54 @@ const ListContainer = ({children}) => {
   )
 }
 
+const UserItem = ({user}) => {
+  return (
+    <div className='flex items-center mx-6 justif-between'>
+      <div className='felx items-center flex-2 text-left'>
+        <Avatar image={user.image} name={user.fullName || user.id} size={32}/>
+      </div>
+    </div>
+  )
+}
+
 const UserList = () => {
+  const {client} = useChatContext()
+  const [user, setUsers] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [listEmpty, setListEmpty] = useState(false)
+
+  useEffect(() => {
+    const getUsers = async () => {
+      if(loading) return
+
+      setLoading(true)
+
+      try {
+        const response = await client.queryUsers(
+          {id: {$ne: client.userID}},
+          {id: 1},
+          {limit: 8}
+        )
+        if(response.users.length) {
+          setUsers(response.users)
+        } else {
+          setListEmpty(true)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+      setLoading(false)
+    }
+    if(client) getUsers()
+  }, [])
+
   return (
     <ListContainer>
-      Userlist
+      {loading ? <div className='font-sm m-20'>Loading Users...</div> : (
+        user?.map((user, i) => (
+          <UserItem index={i} key={user.id} user={user}/>
+        ))
+      )}
     </ListContainer>
   )
 }
