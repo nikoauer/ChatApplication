3c1ab11e05e714aefa9924553b0a524ad19de758
Niko Auer <nikoauer26@gmail.com>
2024-07-12 12:52:14 +1000

    Having issues fixing responsive layout, will use this for the time being fix later on

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import {
-  Dialog,
-  DialogBackdrop,
-  DialogPanel,
-  TransitionChild,
+Transition
 } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import {
+  Bars3Icon,
+  XMarkIcon,
+  ArrowLeftStartOnRectangleIcon,
+} from "@heroicons/react/24/outline";
 import { ChannelList, useChatContext } from "stream-chat-react";
 import {
   ChannelSearch,
@@ -20,17 +21,26 @@ import chatAppLogo from "../assets/ChatAppLogo.png";
 const cookies = new Cookies();
 
 const CompanyHeader = () => (
-  <div className="px-5 pt-3 h-[62px]">
+  <div className="px-5 pt-3 flex justify-center">
     <img src={chatAppLogo} alt="Logo" width="200" />
   </div>
 );
 
-const ChannelListContainer = ({
+const customChannelTeamFilter = (channels) => {
+  return channels.filter((channel) => channel.type === "team");
+};
+
+const customChannelMessagingFilter = (channels) => {
+  return channels.filter((channel) => channel.type === "messaging");
+};
+
+const ChannelListContent = ({
   isCreating,
   setIsCreating,
   setCreateType,
   setIsEditing,
 }) => {
+  const { client } = useChatContext();
   const logout = () => {
     cookies.remove("token");
     cookies.remove("userId");
@@ -43,69 +53,100 @@ const ChannelListContainer = ({
     window.location.reload();
   };
 
+  const filters = { members: { $in: [client.userID] } };
+
+
   return (
-    <div className="max-h-fit">
-      <div className="">
-        <div className="flex flex-col bg-indigo-600 px-6 pb-2 ">
-          <CompanyHeader />
-          <div className="flex flex-col gap-y-4">
-            <div className="pb-3">
-              <ChannelSearch />
-            </div>
-            <div className="flex justify-center items-center">
-              <ChannelList
-                filters={{}}
-                channelRenderFilterFn={() => {}}
-                List={(listProps) => (
-                  <TeamChannelList
-                    {...listProps}
-                    type="team"
-                    isCreating={isCreating}
-                    setIsCreating={setIsCreating}
-                    setCreateType={setCreateType}
-                    setIsEditing={setIsEditing}
-                  />
-                )}
-                Preview={(previewProps) => (
-                  <TeamChannelPreview {...previewProps} type="team" />
-                )}
+
+    <div className="flex flex-col min-w-72 bg-indigo-500 md:w-80 lg:w-96 size-min-w-72 overflow-auto">
+      <CompanyHeader />
+      <div className="flex flex-col justify-center">
+        <div className="mx-3">
+          <ChannelSearch />
+        </div>
+        <div className="flex flex-col my-2">
+        <div className="flex justify-center mx-3 my-1">
+          <ChannelList
+            filters={filters}
+            channelRenderFilterFn={customChannelTeamFilter}
+            List={(listProps) => (
+              <TeamChannelList
+                {...listProps}
+                type="team"
+                isCreating={isCreating}
+                setIsCreating={setIsCreating}
+                setCreateType={setCreateType}
+                setIsEditing={setIsEditing}
               />
-            </div>
-            <div>
-              <ChannelList
-                filters={{}}
-                channelRenderFilterFn={() => {}}
-                List={(listProps) => (
-                  <TeamChannelList
-                    {...listProps}
-                    type="messaging"
-                    isCreating={isCreating}
-                    setIsCreating={setIsCreating}
-                    setCreateType={setCreateType}
-                    setIsEditing={setIsEditing}
-                  />
-                )}
-                Preview={(previewProps) => (
-                  <TeamChannelPreview {...previewProps} type="messaging" />
-                )}
+            )}
+            Preview={(previewProps) => (
+              <TeamChannelPreview {...previewProps} type="team" />
+            )}
+          />
+        </div>
+        <div className="flex justify-center mx-3 my-2">
+          <ChannelList
+            filters={filters}
+            channelRenderFilterFn={customChannelMessagingFilter}
+            List={(listProps) => (
+              <TeamChannelList
+                {...listProps}
+                type="messaging"
+                isCreating={isCreating}
+                setIsCreating={setIsCreating}
+                setCreateType={setCreateType}
+                setIsEditing={setIsEditing}
               />
-            </div>
-          </div>
-          <div className="mt-auto mb-4">
-            <div className="w-[44px] h-[44px] mx-auto rounded-full shadow-md">
-              <div
-                className="font-sans h-full flex items-center justify-center cursor-pointer"
-                onClick={logout}
-              >
-                <img src={signOut} alt="signout" width="50" />
-              </div>
-            </div>
+            )}
+            Preview={(previewProps) => (
+              <TeamChannelPreview {...previewProps} type="messaging" />
+            )}
+          />
           </div>
         </div>
+      </div>
+      <div className="mt-3">
+          <div className="font-sans h-full flex items-center justify-center cursor-pointer">
+            <button
+              type="button"
+              onClick={logout}
+              className="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-24 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              <ArrowLeftStartOnRectangleIcon
+                aria-hidden="true"
+                className="-ml-0.5 h-5 w-5"
+              />
+              Logout
+            </button>
+          </div>
       </div>
       <main className="py-10 lg:pl-72"></main>
     </div>
   );
 };
+
+const ChannelListContainer = ({setCreateType, setIsCreating, setIsEditing}) => {
+  const [toggleContainer, settoggleContainer] = useState(false)
+
+  return (
+    <>
+      <div className="flex h-full shadow-sm">
+        <ChannelListContent 
+          setIsCreating={setCreateType} 
+          setCreateType={setCreateType}
+          setIsEditing={setIsEditing}
+        />
+      </div>
+
+      <div className="hidden">
+      <ChannelListContent 
+          setIsCreating={setCreateType} 
+          setCreateType={setCreateType}
+          setIsEditing={setIsEditing}
+        />
+      </div>
+    </>
+  )
+}
 
 export default ChannelListContainer;
